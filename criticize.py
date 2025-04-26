2d4356b7063acf566170bc7550c8e31a9ab5a8f8
AM <amhfz@protonmail.com>
2025-04-26 17:30:38 -0400

    text edits

--- criticize.py
+++ criticize.py
@@ -5,6 +5,7 @@ Turn harmless labels into reasons you suck
 from transformers import AutoTokenizer, AutoModelForMaskedLM
 import torch
 import numpy as np
+
 tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
 model = AutoModelForMaskedLM.from_pretrained("google-bert/bert-base-uncased")
 # def interpret_label(label:str) -> str:
@@ -31,9 +32,29 @@ model = AutoModelForMaskedLM.from_pretrained("google-bert/bert-base-uncased")
 #         interpretations.add(interpret_label(lbl))
 #     return list(interpretations)
 
-def interpret_labels(regular_labels:list) -> list:
-    judgy_labels = ["dishonour on your cow","wasting money","shenanigans and gallivanting","not calling ","wasting time", "not studying", "reading picture books", "too skinny", "not skinny enough", "visiting other people's houses and showing no manners","is that ur boyfriend/girlfriend?","Why are you out so late?", "you have to make more money", "you have to eat more healthy", ]
-    relevant_judgy_label = []
+
+def interpret_labels(regular_labels: list) -> list:
+    judgy_labels = [
+        "dishonour on your cow",
+        "wasting time",
+        "wasting money",
+        "shenanigans and gallivanting",
+        "not calling enough",
+        "spending too much time with friends",
+        "don't have enough friends",
+        "not studying",
+        "spending too much time on ur phone",
+        "too skinny",
+        "overdue for the dentist",
+        "still not a homeowner",
+        # "not skinny enough",
+        "still not a doctor",
+        "is that ur boyfriend/girlfriend?",
+        "out too late like a criminal",
+        "not making enough money",
+        "not eating healthy enough",
+    ]
+    relevant_judgy_label = ["still not married yet"]
     for judgylabel in judgy_labels:
         jinputs = tokenizer(judgylabel, return_tensors="pt")
         # jlabels = torch.tensor([1]).unsqueeze(0)  # Add batch dimension
@@ -42,19 +63,26 @@ def interpret_labels(regular_labels:list) -> list:
         last_hidden_states = joutputs.hidden_states[-1]  # Get the last hidden states
         # Get the mean of the last hidden states for the label
         label_embedding = last_hidden_states.mean(dim=1).squeeze().detach().numpy()
-        
+
         # Compare with regular labels
-        for regular_label in regular_labels:  
+        for regular_label in regular_labels:
             rinputs = tokenizer(regular_label, return_tensors="pt")
             routputs = model(**rinputs, output_hidden_states=True)
-            rlast_hidden_states = routputs.hidden_states[-1]  # Get the last hidden states
+            rlast_hidden_states = routputs.hidden_states[
+                -1
+            ]  # Get the last hidden states
             # Get the mean of the last hidden states for the regular label
-            regular_label_embedding = rlast_hidden_states.mean(dim=1).squeeze().detach().numpy()
-            
+            regular_label_embedding = (
+                rlast_hidden_states.mean(dim=1).squeeze().detach().numpy()
+            )
+
             # Calculate cosine similarity
-            cosine_similarity = np.dot(label_embedding, regular_label_embedding) / (np.linalg.norm(label_embedding) * np.linalg.norm(regular_label_embedding))
-            
-            if cosine_similarity > 0.685:
+            cosine_similarity = np.dot(label_embedding, regular_label_embedding) / (
+                np.linalg.norm(label_embedding)
+                * np.linalg.norm(regular_label_embedding)
+            )
+
+            if cosine_similarity > 0.67:
                 relevant_judgy_label.append(judgylabel)
         final_list = list(set(relevant_judgy_label))
     return final_list
